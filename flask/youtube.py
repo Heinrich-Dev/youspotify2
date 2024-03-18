8a1b403ec705fbe52e4ee58e77bdde32901aff6c
Heinrich-Dev <boekhoffhenry@gmail.com>
2024-03-17 23:29:38 -0700

    added more functionality, still not fully done

--- flask/youtube.py
+++ flask/youtube.py
@@ -3,8 +3,12 @@
 import os
 
 from googleapiclient.discovery import build
+from pytube import YouTube
 
-def main():
+from moviepy.editor import *
+
+# Returns a list of video IDs from NPR's YouTube channel
+def getNPRIds():
     api_key = os.environ["YOUTUBE_API_KEY"]
     youtube = build("youtube", "v3", developerKey=api_key)
     request = youtube.channels().list(
@@ -26,8 +30,7 @@ def main():
 
     ids = []
 
-    #Always a good sign
-    while True:
+    while True:  #Always a good sign
         request = youtube.playlistItems().list(
             part="contentDetails",
             playlistId=new_playlistId,
@@ -45,21 +48,40 @@ def main():
             new_nextPageToken = response['nextPageToken']
         except KeyError:
             break
-    print(ids)
-    print(len(ids))
-    #TODO: extract video ids from list of videos gathered
-    '''
-    items = response['items']
-    ids = []
-    for item in items:
-        ids.append(item['contentDetails']['videoId'])
-    #print(ids)
-    '''
-    #TODO: check if any video ids are not already downloaded
-    #TODO: given video ids build urls
-    #TODO: given urls download videos from youtube
-    #TODO: given videos from youtube convert to mp3s
-    #TODO: given list of mp3s upload to spotify and create playlist from local files
+    return ids
+
+# Checks video IDs to see if any have already been downloaded, downloads them if not
+def checkVids(ids):
+    toDownload = []
+    file = open('../videos/npr.txt', 'w+')
+    old_ids = file.readlines()
+    for item in ids:
+        if item not in old_ids:
+            file.write(item + '\n')
+            toDownload.append(item)
+    return toDownload
+
+# Downloads and converts videos from youtube given a list of watch IDs
+def downloadandConvertVideos(videos):
+    link = "https://www.youtube.com/watch?v=" + vid
+    yt = YouTube(link)
+    for video in videos:
+        yt.streams.filter(only_audio=True).first().download("../videos")
+
+# Downloads one video from YouTube, used for testing
+def testDownloadandConvert(url):
+    yt = YouTube(url)
+    yt.streams.filter(only_audio=True).first().download("../videos")
+
+#TODO: given videos from youtube convert to mp3s
+#TODO: put all mp3s into a zip file
+#TODO: given list of mp3s upload to spotify and create playlist from local files
+#TODO: have process daemonized and check for updates on NPR's channel
+
+
 
 if __name__ == "__main__":
-    main()
+    #videoIds = getNPRIds()
+    #toDownload = checkVids(videoIds)
+    #downloadandConvertVideos(toDownload)
+    testDownloadandConvert('https://www.youtube.com/watch?v=_pxMTXadCqI')
